9c5e0253b5c12be361c22cb12e11b1f090605a03
Siam Islam <siamislam1603@gmail.com>
2023-07-01 02:31:06 +0600

    import issue fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import Banner from "./Banner/Banner";
 import CreateAndSell from "./CreateAndSell/CreateAndSell";
 import FeaturedCollections from "./FeaturedCollections/FeaturedCollections";
 import Footer from "./Footer/Footer";
-import NFTs from "./NFTS/NFTS";
+import NFTs from "./NFTs/NFTs";
 import TopCollections from "./TopCollections/TopCollections";
 import Transactions from "./Transactions/Transactions";
 
